5aa737dca1300811a5cadab14e23a158c1d32312
sik9252 <sik9252@naver.com>
2023-04-25 23:55:16 +0900

    feat: 게시판, 게시글에서 사용될 공통 컴포넌트 추가

--- pages/board/[contentId].tsx
+++ pages/board/[contentId].tsx
@@ -1,28 +1,18 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import styled from 'styled-components';
-
 import Header from '@/src/components/Header';
-import Footer from '@/src/components/Footer';
+import CommonLayout from '@/src/views/commonLayout';
 
-const FooterSection = styled.div`
-  width: 100%;
-  position: absolute;
-  bottom: 0;
-`;
-
-function contentPage() {
+function Content() {
   const router = useRouter();
   const { contentId } = router.query;
+
   return (
-    <div>
+    <CommonLayout>
       <Header />각 게시판에 있는 게시글의 상세 페이지 {contentId}
-      <FooterSection>
-        <Footer />
-      </FooterSection>
-    </div>
+    </CommonLayout>
   );
 }
 
-export default contentPage;
+export default Content;
