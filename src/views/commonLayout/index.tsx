9f4f5fb4ad42095a71c6e101ecf54c892b10a3e2
sik9252 <sik9252@naver.com>
2023-04-25 23:55:00 +0900

    feat: 게시판, 게시글 페이지에서 사용될 공통 컴포넌트 생성

---
+++ src/views/commonLayout/index.tsx
@@ -0,0 +1,74 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+import {
+  RightSection,
+  RightContentBox,
+  PopularContentBox,
+  PopularContent,
+  BestContent,
+} from './style';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+
+import Footer from '@/src/components/Footer';
+
+const FooterSection = styled.div`
+  width: 100%;
+  bottom: 0;
+`;
+
+type MyComponentProps = {
+  children: ReactNode;
+};
+
+function CommonLayout({ children }: MyComponentProps) {
+  return (
+    <>
+      {children}
+      <RightSection>
+        <PopularContentBox>
+          <div>실시간 인기 글</div>
+          <PopularContent>
+            <div>제목</div>
+            <div>내용</div>
+            <div>
+              <div>게시판</div>
+              <div>
+                <FontAwesomeIcon icon={faThumbsUp} />3
+              </div>
+              <div>
+                <FontAwesomeIcon icon={faComment} />3
+              </div>
+            </div>
+          </PopularContent>
+        </PopularContentBox>
+        <RightContentBox>
+          <div>
+            <p>HOT 게시물</p>
+            <p>더 보기</p>
+          </div>
+          <div>게시글1</div>
+          <div>게시글2</div>
+          <div>게시글3</div>
+          <div>게시글4</div>
+        </RightContentBox>
+        <BestContent>
+          <div>
+            <p>BEST 게시물</p>
+            <p>더 보기</p>
+          </div>
+        </BestContent>
+      </RightSection>
+      
+      <FooterSection>
+        <Footer />
+      </FooterSection>
+    </>
+  );
+  // return <div>{children}공통 컴포넌트 RightSection</div>;
+}
+
+export default CommonLayout;
---
+++ src/views/commonLayout/style.ts
@@ -0,0 +1,144 @@
+import styled from 'styled-components';
+
+export const RightSection = styled.div`
+  width: 325px;
+
+  input {
+    margin-top: 5px;
+  }
+`;
+
+export const RightContentBox = styled.div`
+  height: 190px;
+  margin: 10px 5px;
+  border: 1px solid ${({ theme }) => theme.colors.gray1};
+  background: ${({ theme }) => theme.colors.light_gray1};
+  display: flex;
+  flex-direction: column;
+
+  & > div {
+    height: 100%;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.gray1};
+    padding: 10px;
+    font-size: 13px;
+    cursor: pointer;
+
+    :nth-child(1) {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      & > p {
+        :nth-child(1) {
+          font-size: 15px;
+          color: #3744a5;
+          font-weight: ${({ theme }) => theme.getFontWeight('SB')};
+        }
+        :nth-child(2) {
+          font-size: 12px;
+          color: ${({ theme }) => theme.colors.black3};
+        }
+      }
+    }
+
+    :nth-last-child(1) {
+      border-bottom: none;
+    }
+  }
+`;
+
+export const PopularContentBox = styled.div`
+  margin: 10px 5px;
+  border: 1px solid ${({ theme }) => theme.colors.gray1};
+  background: ${({ theme }) => theme.colors.light_gray1};
+
+  & > div {
+    padding: 10px;
+
+    :nth-child(1) {
+      color: #3744a5;
+      font-weight: ${({ theme }) => theme.getFontWeight('SB')};
+      border-bottom: 1px solid ${({ theme }) => theme.colors.gray1};
+    }
+  }
+`;
+
+export const PopularContent = styled.div`
+  cursor: pointer;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray1};
+  background: ${({ theme }) => theme.colors.light_gray1};
+
+  :nth-last-child(1) {
+    border-bottom: none;
+  }
+
+  & > div {
+    :nth-child(1) {
+      font-size: 15px;
+      font-weight: ${({ theme }) => theme.getFontWeight('SB')};
+    }
+    :nth-child(2) {
+      font-size: 12px;
+      color: ${({ theme }) => theme.colors.black3};
+      padding: 10px 0;
+    }
+    :nth-child(3) {
+      display: flex;
+      & > div {
+        :nth-child(1) {
+          font-size: 12px;
+          color: ${({ theme }) => theme.colors.black3};
+          font-weight: ${({ theme }) => theme.getFontWeight('SB')};
+        }
+        :nth-child(2) {
+          font-size: 12px;
+          color: ${({ theme }) => theme.colors.red};
+          padding: 0 10px;
+
+          & > svg,
+          img {
+            width: 10px;
+            color: ${({ theme }) => theme.colors.red};
+            margin-right: 3px;
+          }
+        }
+        :nth-child(3) {
+          font-size: 12px;
+          color: #00b4d8;
+
+          & > svg,
+          img {
+            width: 10px;
+            color: #00b4d8;
+            margin-right: 3px;
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const BestContent = styled.div`
+  margin: 10px 5px;
+  border: 1px solid ${({ theme }) => theme.colors.gray1};
+  padding: 10px;
+  background: ${({ theme }) => theme.colors.light_gray1};
+
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    & > p {
+      :nth-child(1) {
+        font-size: 15px;
+        color: #3744a5;
+        font-weight: ${({ theme }) => theme.getFontWeight('SB')};
+        cursor: pointer;
+      }
+      :nth-child(2) {
+        font-size: 12px;
+        color: ${({ theme }) => theme.colors.black3};
+        cursor: pointer;
+      }
+    }
+  }
+`;
