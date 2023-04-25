9ee3b339ab761bc7115a9bc55382232353844b36
sik9252 <sik9252@naver.com>
2023-04-25 23:56:26 +0900

    chore: axios, fontawesome 패키지 설치

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination:
-          'https://port-0-nevrytime-be-1maxx2algrqkxj3.sel3.cloudtype.app/:path*',
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       destination:
+  //         'https://port-0-nevrytime-be-1maxx2algrqkxj3.sel3.cloudtype.app/:path*',
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
