184cd8d2c5faf1698d2a7f183057ba6caf4b4d25
sik9252 <sik9252@naver.com>
2023-04-28 00:36:07 +0900

    feat: 마이페이지 퍼블리싱 완료

--- src/views/myPageView/index.tsx
+++ src/views/myPageView/index.tsx
@@ -1,8 +1,48 @@
 /** 마이페이지 뷰 */
+import { Button } from '@/src/components/Button';
 import React from 'react';
 
+import { MyPageContainer, MyInfoBox, MyAccountBox } from './style';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+
 function myPageView() {
-  return <div>회원 탈퇴, 닉네임, 비번 변경</div>;
+  return (
+    <MyPageContainer>
+      <MyInfoBox>
+        <div>
+          <div>내 정보</div>
+          <Button width={64} height={30} borderRadius={15}>
+            로그아웃
+          </Button>
+        </div>
+        <div>
+          <div>
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+          <div>
+            <div>아이디</div>
+            <div>닉네임</div>
+            <div>순천향대</div>
+          </div>
+        </div>
+      </MyInfoBox>
+      <MyAccountBox>
+        <div>계정</div>
+        <div>
+          <Link href="/my-page/password">비밀번호 변경</Link>
+        </div>
+        <div>
+          <Link href="/my-page/nick">닉네임 설정</Link>
+        </div>
+        <div>
+          <Link href="/my-page/with-drawl">회원 탈퇴</Link>
+        </div>
+      </MyAccountBox>
+    </MyPageContainer>
+  );
 }
 
 export default myPageView;
