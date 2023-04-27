59a69c248e7e6acbb582ae254244b4ac0932df11
sik9252 <sik9252@naver.com>
2023-04-27 13:30:46 +0900

    feat: 글 쓴 시각 계산해주는 Hook 생성

---
+++ src/hooks/calcWritedTime.tsx
@@ -0,0 +1,39 @@
+import moment from 'moment';
+
+type contentDataType = {
+  id: number;
+  boardName: string;
+  memberName: string;
+  title: string;
+  content: string;
+  likes: number;
+  createAt: string;
+  image: boolean;
+  show: boolean;
+};
+
+export const calcWritedTime = (data: contentDataType) => {
+  if (
+    Math.ceil(
+      moment.duration(moment().diff(moment(data.createAt))).asMinutes(),
+    ) < 60 &&
+    Math.ceil(
+      moment.duration(moment().diff(moment(data.createAt))).asSeconds(),
+    ) > 59
+  ) {
+    return `${Math.ceil(
+      moment.duration(moment().diff(moment(data.createAt))).asMinutes(),
+    )}분 전`;
+  } else if (
+    Math.ceil(
+      moment.duration(moment().diff(moment(data.createAt))).asMinutes(),
+    ) < 60 &&
+    Math.ceil(
+      moment.duration(moment().diff(moment(data.createAt))).asSeconds(),
+    ) < 60
+  ) {
+    return '방금';
+  } else {
+    return `${moment(data.createAt).format('MM/DD HH:mm')}`;
+  }
+};
