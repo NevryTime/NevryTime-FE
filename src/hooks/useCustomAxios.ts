53a14550cf9e2e88cf636a94e8f1ff306ed640aa
sik9252 <sik9252@naver.com>
2023-04-28 13:26:20 +0900

    feat: axios 사용시 요청과 응답 처리 함수 구현

---
+++ src/hooks/useCustomAxios.ts
@@ -0,0 +1,49 @@
+'use client';
+import { customAxios } from '../axios/customAxios';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
+import { useRefreshToken } from '../hooks/useRefreshToken';
+
+const useAxiosAuth = () => {
+  const { data: session } = useSession();
+  const refreshToken = useRefreshToken();
+
+  useEffect(() => {
+    const requestIntercept = customAxios.interceptors.request.use(
+      (config) => {
+        if (!config.headers['Authorization']) {
+          config.headers[
+            'Authorization'
+          ] = `Bearer ${session?.user?.accessToken}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error),
+    );
+
+    const responseIntercept = customAxios.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const prevRequest = error?.config;
+        if (error?.response?.status === 401 && !prevRequest?.sent) {
+          prevRequest.sent = true;
+          await refreshToken();
+          prevRequest.headers[
+            'Authorization'
+          ] = `Bearer ${session?.user.accessToken}`;
+          return customAxios(prevRequest);
+        }
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      customAxios.interceptors.request.eject(requestIntercept);
+      customAxios.interceptors.response.eject(responseIntercept);
+    };
+  }, [session, refreshToken]);
+
+  return customAxios;
+};
+
+export default useAxiosAuth;
