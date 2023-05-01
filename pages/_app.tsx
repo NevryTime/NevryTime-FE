454fdf24871393286f9a1ec54ac2aa933eeafbc5
sik9252 <sik9252@naver.com>
2023-05-02 02:27:02 +0900

    fix: fontawesome svg 깨지는 현상에 대한 수정 코드 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,10 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 import { RecoilRoot } from 'recoil';
 
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 import Providers from '@/src/utils/Providers';
 
 function App({ Component, pageProps }: AppProps) {
