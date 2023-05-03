daed203a5693c7e7e41b7e2a3708a7bef0392fb9
sik9252 <sik9252@naver.com>
2023-05-03 21:56:19 +0900

    feat: 내가 작성한 글, 댓글 단 글, 스크랩한 글 연동 완료

--- src/views/myCommentArticleView/index.tsx
+++ src/views/myCommentArticleView/index.tsx
@@ -1,79 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 /** components */
 import CommonMyContentListLayout from '../common/commonMyContentListLayout';
 
-/** types */
-type contentListDataType = {
-  id: number;
-  boardName: string;
-  nickName: string;
-  title: string;
-  content: string;
-  likes: number;
-  createAt: string;
-  image: boolean;
-  show: boolean;
-};
+/** axios */
+import useCustomAxios from '@/src/hooks/useCustomAxios';
 
 function MyCommentArticleView() {
-  const [contentList, setContentList] = useState<contentListDataType[]>([
-    {
-      id: 17,
-      boardName: '자유게시판',
-      nickName: 'tester1',
-      title: '게시글 생성 테스트1',
-      content: '내용은 아무거나 게시판 종류는 자유게시판',
-      likes: 0,
-      createAt: '2023-04-27T13:15:56.710734',
-      image: false,
-      show: false,
-    },
-    {
-      id: 16,
-      boardName: '자유게시판',
-      nickName: 'tester1',
-      title: '게시글 생성 테스트1',
-      content: '내용은 아무거나 게시판 종류는 자유게시판',
-      likes: 0,
-      createAt: '2023-04-27T12:18:56.293968',
-      image: false,
-      show: false,
-    },
-    {
-      id: 15,
-      boardName: '자유게시판',
-      nickName: 'tester3',
-      title: '게시글 생성 테스트1',
-      content: '내용은 아무거나 게시판 종류는 자유게시판',
-      likes: 0,
-      createAt: '2023-04-22T08:19:53.678716',
-      image: false,
-      show: false,
-    },
-    {
-      id: 14,
-      boardName: '자유게시판',
-      nickName: 'tester3',
-      title: '게시글 생성 테스트1',
-      content: '내용은 아무거나 게시판 종류는 자유게시판',
-      likes: 0,
-      createAt: '2023-04-27T13:00:53.253813',
-      image: false,
-      show: false,
-    },
-    {
-      id: 13,
-      boardName: '자유게시판',
-      nickName: 'tester3',
-      title: '게시글 생성 테스트1',
-      content: '내용은 아무거나 게시판 종류는 자유게시판',
-      likes: 0,
-      createAt: '2023-04-25T08:19:52.798578',
-      image: false,
-      show: false,
-    },
-  ]);
+  const axios = useCustomAxios();
+  // 내가 댓글 단 게시글
+  const [contentList, setContentList] = useState([]);
+
+  useEffect(() => {
+    const myCommentArticle = async () => {
+      await axios
+        .get(`/api/member/me/comment`)
+        .then((res) => {
+          console.log(res.data);
+          setContentList(res.data.contentList);
+        })
+        .catch((error) => {
+          if (error.response.data.message) {
+            setContentList([]);
+          }
+          console.log(error.response.data.message);
+        });
+    };
+
+    myCommentArticle();
+  }, []);
 
   return (
     <CommonMyContentListLayout
